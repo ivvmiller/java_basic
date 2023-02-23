4b078f5b151f5f8642975eb2070196c3c271ba24
ivvmiller <120119855+ivvmiller@users.noreply.github.com>
2023-02-23 22:12:47 +0700

    Add files via upload
    Задача №2 Java Basic

---
+++ Task2.java
@@ -0,0 +1,10 @@
+package javaBasic;
+
+public class Task2 {
+    public static void main(String[] args) {
+        double var1 = 1353.2;
+        byte var2 = 127;
+        System.out.println(var1);
+        System.out.println(var2);
+    }
+}
